a66a8e8e71c90298246402aa3683169714a190ef
staroffish <ats_jun@163.com>
2023-07-08 22:42:30 +0800

    add tracker server list to bangumi magnet

--- app/spider/internal/biz/spider/bangumispider.go
+++ app/spider/internal/biz/spider/bangumispider.go
@@ -22,6 +22,8 @@ type bangumiTorrent struct {
 	Magnet string `json:"magnet"`
 }
 
+const trackerServerStr = "&tr=https%3A%2F%2Ftr.bangumi.moe%3A9696%2Fannounce&tr=http%3A%2F%2Ftr.bangumi.moe%3A6969%2Fannounce&tr=udp%3A%2F%2Ftr.bangumi.moe%3A6969%2Fannounce&tr=http%3A%2F%2Fopen.acgtracker.com%3A1096%2Fannounce&tr=http%3A%2F%2F208.67.16.113%3A8000%2Fannounce&tr=udp%3A%2F%2F208.67.16.113%3A8000%2Fannounce&tr=http%3A%2F%2Ftracker.ktxp.com%3A6868%2Fannounce&tr=http%3A%2F%2Ftracker.ktxp.com%3A7070%2Fannounce&tr=http%3A%2F%2Ft2.popgo.org%3A7456%2Fannonce&tr=http%3A%2F%2Fbt.sc-ol.com%3A2710%2Fannounce&tr=http%3A%2F%2Fshare.camoe.cn%3A8080%2Fannounce&tr=http%3A%2F%2F61.154.116.205%3A8000%2Fannounce&tr=http%3A%2F%2Fbt.rghost.net%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.publicbt.com%3A80%2Fannounce&tr=http%3A%2F%2Ftracker.prq.to%2Fannounce&tr=http%3A%2F%2Fopen.nyaatorrents.info%3A6544%2Fannounce"
+
 func (m *BangumiSpider) ExtractData(ctx context.Context, webContent string) ([]*spider.AnimeMagnet, error) {
 	m.log.WithContext(ctx).Info("Call BangumiSpider.ExtractData")
 
@@ -38,7 +40,7 @@ func (m *BangumiSpider) ExtractData(ctx context.Context, webContent string) ([]*
 	for _, item := range items.Torrents {
 		animeMagnets = append(animeMagnets, &spider.AnimeMagnet{
 			Name:       item.Title,
-			MagnetLink: item.Magnet,
+			MagnetLink: item.Magnet + trackerServerStr,
 		})
 	}
 
